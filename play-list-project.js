6a79e39825a05c957cb386105570c19e6363d844
Mandy Liu <ikhiiofls@gmail.com>
2026-04-12 22:52:44 -0400

    Rename project references from play-list-project to aero-sports

--- play-list-project.js
+++ play-list-project.js
@@ -7,15 +7,15 @@ import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
- * `play-list-project`
+ * `aero-sports`
  * 
  * @demo index.html
- * @element play-list-project
+ * @element aero-sports
  */
-export class PlayListProject extends DDDSuper(I18NMixin(LitElement)) {
+export class AeroSports extends DDDSuper(I18NMixin(LitElement)) {
 
   static get tag() {
-    return "play-list-project";
+    return "aero-sports";
   }
 
   constructor() {
